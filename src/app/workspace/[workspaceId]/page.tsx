6d2a745d56382b7c060dc94c31ff324a168fafea
hi2suresh <hi2suresh@gmail.com>
2025-01-16 12:12:00 +1100

    completed toolbar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,9 +1,10 @@
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+'use client';
 
-export default function WorkspaceIdPage({ params }: WorkspaceIdPageProps) {
-  return <div>{params.workspaceId}</div>;
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+
+export default function WorkspaceIdPage() {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+  return <div>Data: {JSON.stringify(data)}</div>;
 }
